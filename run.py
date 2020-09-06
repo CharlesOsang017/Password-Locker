80b62323ebbf17f0da53ecd43a0fa1b10e21f46e
CharlesOsango17 <charlesosango02@gmail.com>
2020-09-06 10:21:52 +0300

    Created function main

--- run.py
+++ run.py
@@ -37,80 +37,80 @@ def check_existing_passwords(number):
 def display_passwords():
        return User.display_passwords()
 
-# def main():
-#     print("Hello Welcome to  Password-Locker. What is your name?")
-#     password_name = input()
-#
-#     print(f"Hello {password_name}. Select what you would  like to do?")
-#     print('\n')
-#
-#     while True:
-#                     print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
-#
-#                     short_code = input().lower()
-#
-#                     if short_code == 'cc':
-#                             print("New Contact")
-#                             print("-"*10)
-#
-#                             print ("First name ....")
-#                             f_name = input()
-#
-#                             print("Last name ...")
-#                             l_name = input()
-#
-#                             print("Phone number ...")
-#                             p_number = input()
-#
-#                             print("Email address ...")
-#                             e_address = input()
-#
-#
-#                             save_passwords(create_password(f_name,l_name,p_number,e_address)) # create and save new contact.
-#                             print ('\n')
-#                             print(f"New Contact {f_name} {l_name} created")
-#                             print ('\n')
-#
-#                     elif short_code == 'dc':
-#
-#                             if display_passwords():
-#                                     print("Here is a list of all your contacts")
-#                                     print('\n')
-#
-#                                     for password in display_passwords():
-#                                      print(f"Name: {password.first_name} {password.last_name}")
-#                                      print(f"Phone number: {password.phone_number}")
-#                                      print(f"Email address: {password.email}")
-#
-#
-#
-#                                     print('\n')
-#                             else:
-#                                     print('\n')
-#                                     print("You dont seem to have any contacts saved yet")
-#                                     print('\n')
-#
-#                     elif short_code == 'fc':
-#
-#                             print("Enter the number you want to search for")
-#
-#                             search_number = input()
-#                             if check_existing_passwords(search_number):
-#                                     search_password = find_password(search_number)
-#                                     print(f"{search_password.first_name} {search_password.last_name}")
-#                                     print('-' * 20)
-#
-#                                     print(f"Phone number.......{search_password.phone_number}")
-#                                     print(f"Email address.......{search_password.email}")
-#                             else:
-#                                     print("That contact does not exist")
-#
-#                     elif short_code == "ex":
-#                             print("Thank you for using Password-Locker..Good Bye .......")
-#                             break
-#                     else:
-#                             print("I really didn't get that. Please use the short codes")
-#
+def main():
+    print("Hello Welcome to  Password-Locker. What is your name?")
+    password_name = input()
+
+    print(f"Hello {password_name}. Select what you would  like to do?")
+    print('\n')
+
+    while True:
+                    print("Use these short codes : cc - create a new contact, dc - display contacts, fc -find a contact, ex -exit the contact list ")
+
+                    short_code = input().lower()
+
+                    if short_code == 'cc':
+                            print("New Contact")
+                            print("-"*10)
+
+                            print ("First name ....")
+                            f_name = input()
+
+                            print("Last name ...")
+                            l_name = input()
+
+                            print("Phone number ...")
+                            p_number = input()
+
+                            print("Email address ...")
+                            e_address = input()
+
+
+                            save_passwords(create_password(f_name,l_name,p_number,e_address)) # create and save new contact.
+                            print ('\n')
+                            print(f"New Contact {f_name} {l_name} created")
+                            print ('\n')
+
+                    elif short_code == 'dc':
+
+                            if display_passwords():
+                                    print("Here is a list of all your contacts")
+                                    print('\n')
+
+                                    for password in display_passwords():
+                                     print(f"Name: {password.first_name} {password.last_name}")
+                                     print(f"Phone number: {password.phone_number}")
+                                     print(f"Email address: {password.email}")
+
+
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any contacts saved yet")
+                                    print('\n')
+
+                    elif short_code == 'fc':
+
+                            print("Enter the number you want to search for")
+
+                            search_number = input()
+                            if check_existing_passwords(search_number):
+                                    search_password = find_password(search_number)
+                                    print(f"{search_password.first_name} {search_password.last_name}")
+                                    print('-' * 20)
+
+                                    print(f"Phone number.......{search_password.phone_number}")
+                                    print(f"Email address.......{search_password.email}")
+                            else:
+                                    print("That contact does not exist")
+
+                    elif short_code == "ex":
+                            print("Thank you for using Password-Locker..Good Bye .......")
+                            break
+                    else:
+                            print("I really didn't get that. Please use the short codes")
+
 # if __name__ == '__main__':
 #     main()
 #
