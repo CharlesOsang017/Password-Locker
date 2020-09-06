f30a38fab07f72a61029d4cd676a4feb1b30a05b
CharlesOsango17 <charlesosango02@gmail.com>
2020-09-06 10:36:26 +0300

    Creted a README.md file

--- run.py
+++ run.py
@@ -111,6 +111,6 @@ def main():
                     else:
                             print("I really didn't get that. Please use the short codes")
 
-# if __name__ == '__main__':
-#     main()
-#
+if __name__ == '__main__':
+    main()
+
